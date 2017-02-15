fc641c16d87d1ae8b241244b08f498cff9c1c1f5
Maddie Ostergaard <Mostergaard@stuy.edu>
2017-02-14 23:45:47 -0500

    updating canvas.js

--- canvas.js
+++ canvas.js
@@ -27,22 +27,23 @@ var add_Circle = function(e){
     
     var x = e.clientX;
     var y = e.clientY;
-    var x1 = x-5;
-    var y1 = y-55;
+    var x1 = x;
+    var y1 = y;
 
     //the lines aren't connecting the circles perfectly at the center
     //can this be fixed?
     ctx.arc(x1,y1,25,0,2*Math.PI);
     ctx.moveTo(x1,y1);
     
-    // ctx.fill();
+    ctx.fillStyle = '#ff00ff'
+    ctx.fill();
     // the above fill function fills in the whole object with color weirdly
     // can we get ONLY the circles to fill with color?
     ctx.stroke();
     
     
 }
-    
+
 
 
 slate.addEventListener("click", add_Circle);
